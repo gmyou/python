b743ce4405b6e1233e54956bdc404b513f7a1dc0
You Gun-Myoung <gmyou@Youui-MacBook-Air.local>
2016-08-21 13:53:11 +0900

    add datediff on calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 # date
-import datetime
-d = datetime.date.today()
+from datetime import date
+d = date.today()
 print d
 print d.isoformat()
 print d.ctime()
@@ -12,5 +12,21 @@ from time import localtime, strftime
 print strftime("%Y-%m-%d %H:%M:%S")
 print strftime("%Y-%m-%d %H:%M:%S", localtime())
 
-# calc
-# print d+2
+# datediff
+d0 = date(2008, 8, 18)
+d1 = date(2008, 9, 26)
+delta = d0 - d1
+print delta.days
+
+from datetime import datetime
+date_format = "%m/%d/%Y"
+a = datetime.strptime('8/18/2008', date_format)
+b = datetime.strptime('9/27/2008', date_format)
+delta = b - a
+print delta.days # that's it
+
+date_format = "%Y-%m-%d %H:%M"
+a = datetime.strptime('2016-08-19 13:49', date_format)
+b = datetime.strptime('2016-08-21 13:49', date_format)
+delta = b - a
+print delta.days # that's it
