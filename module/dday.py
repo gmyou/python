84dac8fd26a9ac37052566f8c172814b0e28cb89
Jack You <gmyou71@gmail.com>
2016-08-22 12:22:56 +0900

    modified dday.py

--- module/dday.py
+++ module/dday.py
@@ -1,8 +1,32 @@
 from datetime import datetime, timedelta
+import sys, getopt
 
-def dday(day, hour):
+def dday(argv):
     now = datetime.now()
-    dday = now + timedelta(days=day) + timedelta(hours=hour)
-    print now.ctime(), ' 1day 20hours increament day is ', dday.ctime()
+
+    try:
+        opts, args = getopt.getopt(argv, "d:h:", "day=")
+    except getopt.GetoptError:
+        print "dday.py -d <days> -h <hours>"
+
+    print now.ctime()
+    _dday = now
+    # print _dday
+    for opt, arg in opts:
+        # print opt, arg
+        # pass
+
+        if opt == '-d':
+            _dday = _dday + timedelta(days=int(arg))
+            print '+', arg, ' days '
+        if opt == '-h':
+            _dday = _dday + timedelta(hours=int(arg))
+            print '+', arg, ' hours '
+
+    print _dday.ctime()
 
 # dday(1, 10)
+#print sys.argv[2]
+#dday(int(sys.argv[1]), int(sys.argv[2]))
+if __name__ == "__main__":
+    dday(sys.argv[1:])
